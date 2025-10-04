db4098a95bd3c06f2e5239a12673442718ed23ed
root <joel.reujoe@gmail.com>
2025-10-04 10:51:54 +0530

    added changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,40 +9,6 @@ provider "google" {
 
 
 
-
-resource "google_cloud_run_service" "fastapi" {
-  name     = "fastapi-cicd"
-  location = var.region
-
-  template {
-    spec {
-      containers {
-        image = "${var.region}-docker.pkg.dev/${var.project_id}/fastapi-cicd/fastapi-cicd:latest"
-        ports {
-          container_port = 8000
-        }
-      }
-
-    }
-  }
-
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
-}
-
-resource "google_cloud_run_service_iam_member" "public" {
-  location = var.region
-  project  = var.project_id
-  service  = google_cloud_run_service.fastapi.name
-
-  role   = "roles/run.invoker"
-  member = "allUsers"
-}
-
-
-
 resource "google_artifact_registry_repository" "fastapi_repo" {
   provider      = google
   location      = var.region
