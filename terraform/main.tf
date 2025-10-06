05aaa99d3e720bcd8edfaf26f2d342610911e9e1
root <joel.reujoe@gmail.com>
2025-10-06 11:37:51 +0530

    added ci cd level changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,11 +8,31 @@ provider "google" {
 }
 
 
+resource "google_cloud_run_service" "default" {
+  name     = "fastapi-service"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/${var.project_id}/fastapi-repo/fastapi-repo:latest"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+  depends_on = [google_artifact_registry_repository.fastapi_repo]
+}
+
 
 resource "google_artifact_registry_repository" "fastapi_repo" {
   provider      = google
   location      = var.region
-  repository_id = "fastapi-cicd"
+  repository_id = "fastapi-service"
   description   = "Docker repo for FastAPI CI/CD app"
   format        = "DOCKER"
 }
